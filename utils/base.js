97ecdcae1bac1cc0a4436f4c5885e3befc4867c0
asrocky <asrocky@yeah.net>
2015-12-10 01:14:14 +0800

    update base.js

--- utils/base.js
+++ utils/base.js
@@ -1,126 +1,94 @@
-define(['jquery'],function($){	
+"use strict";
 
-	'use strict';
-	
-	function Base(){
-		this.cfg = {
-			debug : true
-		};
-		//存储事件与函数的映射关系
-		this.evtFns = {};
-		//存储事件与实现函数的映射关系
-		this.fnImpl = {};
+define(["jquery"],function($){	
 
-		var sNote = "<!-- >>>>>>>>>>>>>>>>>>>>>>> @note >>>>>>>>>>>>>>>>>>>>>>> -->",
-			eNote = "<!-- <<<<<<<<<<<<<<<<<<<<<<< @note <<<<<<<<<<<<<<<<<<<<<<< -->";
+	var _sNote = "<!-- >>> @note >>> -->",
+		_eNote = "<!-- <<< @note <<<-->";
 
-		this.myHtml = function(oriFn){
-			return function(){
-				var th = this; //这里的this指的是调用者
-				oriFn.apply(th, arguments);
-				if(arguments[1]!==undefined){
-					var file = arguments[1];
-					$(th).before(sNote.replace('@note',file));
-					$(th).after(eNote.replace('@note',file));
-				}
+	var _evtFns = {},
+		_fnImpl = {};
+
+	//覆写jquery的html方法
+	$.prototype.html = function(oriHtml){
+		return function(){
+			var self = this;
+			oriHtml.apply(self,arguments);
+			if(arguments[1]!==undefined){
+				var fileName = arguments[1];
+				$(self).prepend(_sNote.replace(/@note/,fileName));
+				$(self).append(_eNote.replace(/@note/,fileName));
 			}
-		}($.prototype.html);
-
-		this.myPrepend = function(oriFn){
-			return function(){
-				var th = this; //这里的this指的是调用者
-				oriFn.apply(th,arguments[0]);
-				if(arguments[1]!==undefined){
-					var file = arguments[1];
-					$(arguments[0]).before(sNote.replace('@note',file));
-					$(arguments[0]).after(eNote.replace('@note',file));
-				}
-			}
-		}($.prototype.prepend);
-
-		//覆写jquery的方法
-		if(this.cfg.debug){
-			$.prototype.html = this.myHtml;
-			$.prototype.prepend = this.myPrepend;
 		}
+	}($.prototype.html);
 
-		//绑定事件函数（多个）
-		this.bind = function(key, fns){
-			var self = this;
-			if(fns instanceof Array){
-				for(var i in fns){
-					self.doBind(key,fns[i]);
-				}
-			}else{
-				self.doBind(key,fns);
-			}
-		};
-
-		//绑定事件函数
-		this.doBind = function(key,fn){
-			if(!(fn instanceof Function))
-				return;
-			var self = this;
-			if( !self.evtFns[key] ){
-				self.evtFns[key] = [];
-			}
-			self.evtFns[key].push(fn);
-		};
-
-		//解除事件函数绑定
-		this.unBind = function(key,fn){
-			var self = this;
-			var fns = self.evtFns[key];
-			if(!fns){
-				return false;
-			}
-			if(!fn){
-				fns && (fns.length=0);
-			}else{
-				for(var i=0,_fn; _fn=fns[i++];){
-					if(_fn===fn){
-						fns.splice(i,1);
-					}
-				}
-			}
-		};
-
-		//消息发布
-		this.trigger = function(){
-			var self = this;
-			var key = Array.prototype.shift.call(arguments);
-			var fns = self.evtFns[key];
-			if(!fns || fns.length===0){
-				return false;
-			}
-			for(var i=0,fn; fn=fns[i++];){
-				fn.apply(this,arguments);
-			}
-		};
-
-		//实现事件函数
-		this.impl = function(key,fn){
-			if(!(fn instanceof Function)){
-				console.log('fn is not a Function.');
-				return false;
-			}
-			var self = this;
-			self.fnImpl[key] = fn;
-		};
-
-		//请求事件
-		this.request = function(){
-			var self = this;
-			var key = Array.prototype.shift.call(arguments);
-			var fn = self.fnImpl[key];
-			if(fn instanceof Function){
-				return fn.apply(this, arguments);
-			}else{
-				return false;
-			}
-		};
+	//触发消息(一个消息对应多个事件)
+	function trigger(){
+		var key = Array.prototype.shift.call(arguments);
+		var fns = _evtFns[key];
+		if(!fns || 0===fns.length){
+			return false;
+		}
+		for(var i=0,fn; fn=fns[i++];){
+			fn.apply(this, arguments);
+		}
 	}
-	
-	return new Base();
-	
+
+	//请求消息(一个消息对应一个实现)
+	function request(){
+		var key = Array.prototype.shift.call(arguments);
+		var fn  = _fnImpl[key];
+		if(fn instanceof Function){
+			return fn.apply(this, arguments);
+		}else{
+			return false
+		}
+	}
+
+	//绑定事件函数（多个）
+	function bind(key, fns){
+		if(fns instanceof Array){
+			for(var i in fns){
+				doBind(key, fns[i]);
+			}
+		}else{
+			doBind(key, fns);
+		}
+	}
+
+	//绑定事件函数
+	function doBind(key, fn){
+		if(!(fn instanceof Function))
+			return;
+		_evtFns[key] = _evtFns[key] || [];
+		_evtFns[key].push(fn);
+	}
+
+	//解除事件函数绑定
+	function unBind(key, fn){
+		var fns = _evtFns[key];
+		if(!fns) return;
+		if(!fn){
+			fns && (fns.length=0);
+		}else{
+			for(var i=0,_fn; _fn=fns[i++];){
+				if(_fn===fn) fns.splice(i,1);
+			}
+		}
+	}
+
+	//实现请求消息
+	function impl(key,fn){
+		if(!fn instanceof Function){
+			console.log("fn is not a Function.");
+		}
+		_fnImpl[key] = fn;
+	}
+
+	return {
+		trigger	: trigger,
+		request : request,
+		bind 	: bind,
+		unBind 	: unBind,
+		impl 	: impl
+	}
 });
